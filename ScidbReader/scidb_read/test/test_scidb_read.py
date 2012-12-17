ab7bde07d6d70f94d2947d7bbae97b0e7cf40f58
dllahr <github.20.dllahr@xoxy.net>
2012-12-17 07:47:44 -0500

    added comments

--- ScidbReader/scidb_read/test/test_scidb_read.py
+++ ScidbReader/scidb_read/test/test_scidb_read.py
@@ -17,25 +17,33 @@ class TestScidbReader(unittest.TestCase):
     def setUp(self):
         unittest.TestCase.setUp(self)
         
-        commands.getoutput("./setup.sh {}".format(self.array_name))
+#        commands.getoutput("./setup.sh {}".format(self.array_name))
         
         
     def tearDown(self):
-        commands.getoutput("iquery -naq \"remove({})\"".format(TestScidbReader.array_name))
+        unittest.TestCase.tearDown(self)
+#        commands.getoutput("iquery -naq \"remove({})\"".format(TestScidbReader.array_name))
         
         
     def test_read(self):
+        #connect to SciDB database
         scidb = scidbapi.connect("localhost", 1239)
         
+        #instantiate reader by passing reference to database connection
         reader = scidb_read.scidb_reader.ScidbReader(scidb)
         
+        #read for a specific query - in this case all the data in a previously setup array
         reader.read("scan({})".format(TestScidbReader.array_name))
         
+        #iterate over the data returned by the query, printing it out
+        #(data is returned as a list)
         for data in reader:
             print data
             
+        #end the query
         reader.complete_query()
         
+        #close the database connection
         scidb.disconnect()
 
 
